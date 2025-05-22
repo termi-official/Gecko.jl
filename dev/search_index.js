32c476b12064ab05842ec4b81d3fa41520c6703f
Documenter.jl <documenter@juliadocs.github.io>
2025-05-22 08:30:33 +0000

    build based on 211182b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Gecko","page":"Home","title":"Gecko","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Gecko.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#Gecko.GeckoGraph","page":"Home","title":"Gecko.GeckoGraph","text":"GeckoGraph([nnodes])\n\nGecko multilevel graph for linear ordering problems.\n\n\n\n\n\n","category":"type"}]
+}
